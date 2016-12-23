ac6e338364a6788f4993772150e7798b053c78e1
Mike McGookey <mikemcgo@umich.edu>
2016-12-23 00:18:20 -0500

    Data now is entered into database Work on data quality next, as well as ?? convention in preparation for webapp

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Thoughts on what needs to be done
  - Unparseable data may have to be hand corrected
  - Need to establish convention for dates with ??, as there are a large number of them 
  - Need to set refresh convention, check update times on webpages
+ - Creating a local instance of mongodb from https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
+ - Code only runs on full version of ubuntu 16.04 in virtualbox for some reason
+ - Pimpmuckl's askfm link is formatted as ask.fm, which cannot be inserted into mongo as a field name, so it has been edited manually
+ - On machine restart, be sure to restart mongod service
+
+ # Real ToDo
+ - Establish convention for ?? in data before database entry
+ - Cleanup data quality
+ - Fix Birthdays (last priority honestly)
--- src/data_parsing.py
+++ src/data_parsing.py
@@ -5,6 +5,13 @@
 import os, re
 from operator import itemgetter
 from parse import *
+from pymongo import MongoClient
+
+db_client = MongoClient()
+db = db_client.dota
+db.players.remove()
+
+
 
 # Set directory parameters
 DATA_DIRECTORY = "data/"
@@ -12,6 +19,7 @@ RESULTS_DIRECTORY = "results/"
 
 # Establish a file for tracking pages that cause issues, mostly personalities
 nil_history_pages = open(RESULTS_DIRECTORY + "0_error_pages.txt", 'w')
+history_error_pages = open(RESULTS_DIRECTORY + "0_noneerror_pages.txt", 'w')
 
 for file_name in os.listdir(DATA_DIRECTORY):
 	try:
@@ -24,29 +32,37 @@ for file_name in os.listdir(DATA_DIRECTORY):
 		removed_top = body.split("{{Infobox player")[1].split("|history=")
 		simple_vars = removed_top[0]
 		history = removed_top[1].rpartition("}}")[0]
-
-		# Retrieve the key value pairs in the nonstandard data format
-		player_attrs = {}
-		for line in simple_vars.split("\n"):
-			if '=' in line:
-				key_value_pair = line.replace('|', '',1).split("=")
-				player_attrs.update({key_value_pair[0]: key_value_pair[1]})
-
-		# Obtain the history of the players, using parse to retrieve the data
-		# This method is not robust, but I don't feel like learning regex at the moment
-		# A number of None entries are appearing, due to nonstandard data formats (270/3972 lines are bad)
-		for line in history.split("\n"):
-			# http://stackoverflow.com/questions/11844986/convert-string-to-variables-like-format-but-in-reverse-in-python
-			if "{{TH|" in line:
-				line = line.replace('{', '').replace('}', '')
-				m = parse("TH|{} — {}|{}", line)
-				print(m)
-
-		# run a normal parse (assume resonable data)
-		# Attempt to account for dota1/starcraft data
-		# push unparseable data to files to be manually edited
 	except IndexError:
 		# These files had no history listed
 		nil_history_pages.write(file_name + "\n")
 
+	# Retrieve the key value pairs in the nonstandard data format
+	player_attrs = {}
+	for line in simple_vars.split("\n"):
+		if '=' in line:
+			key_value_pair = line.replace('|', '',1).split("=")
+			if key_value_pair[1]:
+				player_attrs.update({key_value_pair[0]: key_value_pair[1]})
+
+	player_attrs['history'] = []
+	# Obtain the history of the players, using parse to retrieve the data
+	# This method is not robust, but I don't feel like learning regex at the moment
+	# A number of None entries are appearing, due to nonstandard data formats (270/3972 lines are bad)
+	for line in history.split("\n"):
+		# http://stackoverflow.com/questions/11844986/convert-string-to-variables-like-format-but-in-reverse-in-python
+		if "{{TH|" in line:
+			line = line.replace('{', '').replace('}', '')
+			history_element = parse("TH|{} — {}|{}", line)
+			if history_element:
+				player_attrs['history'].append((history_element[0], history_element[1], history_element[2]))
+			else:
+				history_error_pages.write(file_name + ":" + line + "\n")
+		
+	output_file = open(RESULTS_DIRECTORY + file_name, 'w')
+	output_file.write(str(player_attrs))
+	output_file.close()
+
+	db.players.insert(player_attrs)
+
+history_error_pages.close()
 nil_history_pages.close()
